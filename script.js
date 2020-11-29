93640e25b6576973d24e07c24645c2bf032390ae
galaxydeve <devphs513@gmail.com>
2020-11-30 00:21:05 +0800

    minor update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 try {
   // define toolbar buttons to show
+  // if this value is undefined or its length is 0, default toolbar buttons will be shown
   const buttons = [
     'select',
     'shapes',
@@ -16,7 +17,8 @@ try {
     'clear'
   ];
 
-  // custom shapes
+  // define custom shapes
+  // if this value is undefined or its length is 0, default shapes will be used
   const shapes = [
     `<svg viewBox="-10 -10 180 180" fill="none" stroke="none" stroke-linecap="square" stroke-miterlimit="10"><path stroke="#000000" stroke-width="8" stroke-linecap="butt" d="m0 0l25.742783 0l0 0l38.614174 0l90.09974 0l0 52.74803l0 0l0 22.6063l0 15.070862l-90.09974 0l-61.5304 52.813744l22.916225 -52.813744l-25.742783 0l0 -15.070862l0 -22.6063l0 0z" fill-rule="evenodd"></path></svg>`,
     `<svg viewBox="-10 -10 180 180" fill="none" stroke="none" stroke-linecap="square" stroke-miterlimit="10"><path stroke="#000000" stroke-width="8" stroke-linejoin="round" stroke-linecap="butt" d="m1.0425826 140.35696l25.78009 -49.87359l0 0c-30.142242 -17.309525 -35.62507 -47.05113 -12.666686 -68.71045c22.958385 -21.65932 66.84442 -28.147947 101.387596 -14.990329c34.543175 13.1576185 48.438576 41.655407 32.10183 65.83693c-16.336761 24.181526 -57.559166 36.132935 -95.233955 27.61071z" fill-rule="evenodd"></path></svg>`,
@@ -24,7 +26,11 @@ try {
   ];
 
   var imgEditor = new ImageEditor('#image-editor-container', buttons, shapes);
-  console.log('initialize image editor')
+  console.log('initialize image editor');
+
+  // let status = imgEditor.getCanvasJSON();
+  // imgEditor.setCanvasStatus(status);
+
 } catch (_) {
   const browserWarning = document.createElement('div')
   browserWarning.innerHTML = '<p style="line-height: 26px; margin-top: 100px; font-size: 16px; color: #555">Your browser is out of date!<br/>Please update to a modern browser, for example:<a href="https://www.google.com/chrome/" target="_blank">Chrome</a>!</p>';
